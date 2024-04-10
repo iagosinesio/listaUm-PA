f6e63c5bd71165677f2dfc1bf2a5ec3258911519
Iago Bayesiano <38022035+iagosinesio@users.noreply.github.com>
2024-04-10 15:23:13 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+	public int somar(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtracao(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+	
+	public int dividir(int a, int b) {
+		return a / b;
+	}
+}
